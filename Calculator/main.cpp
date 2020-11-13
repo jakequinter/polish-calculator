3b5a9e4e5033ad11eb7ddcde27ea1503d37990af
jakequinter <500132870@fvtc.edu>
2020-11-13 11:50:55 -0600

    Initial Commit

---
+++ Calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Calculator
+//
+//  Created by Jake Quinter on 11/13/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
